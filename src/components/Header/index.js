ba57b4870db82de1a054a6f1a2e74ce762a1dc46
Asad <asadkhakimov05@gmail.com>
2023-07-17 12:52:35 +0600

    MASTER

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          header
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
